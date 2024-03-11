c1ac8d8a8b35a6f1d70d541688fde41be91fc7b3
lata <imlata1111@gmail.com>
2024-03-11 11:49:30 +0530

    working-on-training-model

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 import telebot
-API_KEY='6410553908:AAFPXhYc8Yh0jcs-w_U1qIpuYI2RCkKSCHA'
+from neural_intents import GenericAssistant
+import sys
+from io import StringIO
+import pandas as pd
+
+
+API_KEY = '6410553908:AAFPXhYc8Yh0jcs-w_U1qIpuYI2RCkKSCHA'
 
 bot = telebot.TeleBot(API_KEY, parse_mode=None)
 
@@ -11,27 +17,49 @@ def send_hello_message(msg):
 # borrow loan
 
 
-@bot.message_handler(commands=["loan", "borrow"])
+@bot.message_handler(commands=["loan", "borrow, credit"])
 def borrow_loan(msg):
     loan_amount = bot.reply_to(msg, "How much do you want to borrow?")
-    bot.register_next_step_handler(loan_amount, waiting_time)
+    bot.register_next_step_handler(loan_amount, borrow_loan_amount)
 
-def waiting_time(msg):
-    waiting_time = bot.reply_to(msg, "How many hours are you willing to wait for me to find a lender?")
-    bot.register_next_step_handler(waiting_time, waiting_time_handler)
-    
-def waiting_time_handler(msg):
-    waiting_time=msg.text 
-    bot.reply_to(
-        msg, f"Ok! I will try to find a lender within {waiting_time} hours.")
 
-def borrow_loan_amount_handler(msg):
+def borrow_loan_amount(msg):
     loan_amount = msg.text
     bot.reply_to(
-        msg, f"Your loan request of {loan_amount} rupees is under process.")
-    
+        msg, f"Your loan request of {loan_amount} rupees is under process. You will be informed within 30 minutes.")
 
-#send notification
+
+def bye(msg):
+    bot.send_message(msg.chat.id, "Goodbye!")
+    sys.exit(0)
+
+
+def thanks(msg):
+    bot.send_message(msg.chat.id, "You're welcome:)")
+
+
+def default_handler(msg):
+    bot.reply_to(msg, "I did not understand.")
+
+
+# send notification
+
+# create group
+
+#mapping
+mappings = {
+    'wants_loan': borrow_loan,
+    'wants_amount': borrow_loan_amount,
+    'bye': bye,
+    None: default_handler
+}
+
+# training model
+assistant = GenericAssistant(
+    'src/intents.json', mappings, "peer_to_peer_lending_bot")
+
+assistant.train_model()
+assistant.save_model()
 
 
 bot.polling()
