865faa813ebd78ca823fde715bda69aed9b986be
Rizul Gupta <mail2rizul@gmail.com>
2024-03-14 23:22:17 +0530

    refactor code
    Signed-off-by: Rizul Gupta <mail2rizul@gmail.com>

--- src/main.py
+++ src/main.py
@@ -29,24 +29,6 @@ def send_greet(msg):
     bot.reply_to(msg, "Hello! This is a peer-to-peer lending bot!")
 
 # borrow loan
-def borrow_loan(msg):
-    user_id = msg.from_user.id
-
-    loan_msg = bot.reply_to(msg, "How much money do you want to borrow?")
-    bot.register_next_step_handler(loan_msg, lambda msg: process_loan_request(msg, user_id, group_name))
-
-
-def process_loan_request(msg, user_id, group_id):
-    loan_amount = extract_numeric_value(msg.text)
-
-    if loan_amount is not None:
-        response = f"Your loan request of {loan_amount} rupees is under process. You will be informed within 30 minutes."
-        bot.reply_to(msg, response)
-
-        send_loan_notification(group_id, user_id, loan_amount)
-    else:
-        bot.reply_to(msg, "Invalid amount. Please enter a numeric value greater than zero.")
-        borrow_loan(msg)
 
 
 def extract_numeric_value(sentence):
@@ -63,33 +45,6 @@ def bye(msg):
     bot.stop_polling()
     sys.exit(0)
 
-def add_to_group_request(msg):
-    user_id = msg.from_user.id
-    username = msg.from_user.username  
-    bot.send_message(user_id, "Please enter the name of the group you want to join:")
-    bot.register_next_step_handler(msg, lambda msg: process_group_name_for_join(msg, user_id, username))
-
-def process_group_name_for_join(msg, user_id, username):
-    group_name = msg.text
-    if is_group_exists(group_name):
-        bot.send_message(user_id, "Please enter the join code for the group:")
-        bot.register_next_step_handler(msg, lambda msg: process_join_code_for_join(msg, user_id, username, group_name))
-    else:
-        bot.send_message(user_id, f"Group '{group_name}' does not exist.")
-
-def process_join_code_for_join(msg, user_id, username, group_name):
-    join_code = msg.text
-    if is_join_code_correct(group_name, join_code):
-        send_request_to_admin(group_name, user_id, username)
-    else:
-        bot.send_message(user_id, "Incorrect join code. Please try again.")
-
-def send_request_to_admin(group_name, user_id, username):
-    admin_id = get_admin_id(group_name)
-    if admin_id is not None:
-        notification_msg = f"User @{username} wants to join the group '{group_name}'. Do you approve?"  # Send username to admin
-        bot.send_message(admin_id, notification_msg)
-
 def thanks(msg):
     bot.send_message(msg.chat.id, "You're welcome:)")
 
@@ -98,7 +53,24 @@ def default_handler(msg):
     bot.reply_to(msg, "I did not understand.")
 
 
-# send notification
+# loan notification
+def borrow_loan(msg):
+    user_id = msg.from_user.id
+    loan_msg = bot.reply_to(msg, "How much money do you want to borrow?")
+    bot.register_next_step_handler(loan_msg, lambda msg: process_loan_request(msg, user_id, group_name))
+
+def process_loan_request(msg, user_id, group_id):
+    loan_amount = extract_numeric_value(msg.text)
+
+    if loan_amount is not None:
+        response = f"Your loan request of {loan_amount} rupees is under process. You will be informed within 30 minutes."
+        bot.reply_to(msg, response)
+
+        send_loan_notification(group_id, user_id, loan_amount)
+    else:
+        bot.reply_to(msg, "Invalid amount. Please enter a numeric value greater than zero.")
+        borrow_loan(msg)
+
 def send_loan_notification(group_id, sender_id, loan_amount):
     members = get_group_members(group_id)
     notification_msg = f"User {sender_id} has requested a loan of {loan_amount} rupees. Do you want to give him the loan? "
@@ -129,6 +101,36 @@ def process_password(msg, user_id, group_name, join_code):
     group_creation(group_name, user_id, join_code, password) 
 
     bot.send_message(user_id, f"Group '{group_name}' created successfully with join code: {join_code} and you are the admin for that group")
+
+
+# join group 
+def add_to_group_request(msg):
+    user_id = msg.from_user.id
+    username = msg.from_user.username  
+    bot.send_message(user_id, "Please enter the name of the group you want to join:")
+    bot.register_next_step_handler(msg, lambda msg: process_group_name_for_join(msg, user_id, username))
+
+def process_group_name_for_join(msg, user_id, username):
+    group_name = msg.text
+    if is_group_exists(group_name):
+        bot.send_message(user_id, "Please enter the join code for the group:")
+        bot.register_next_step_handler(msg, lambda msg: process_join_code_for_join(msg, user_id, username, group_name))
+    else:
+        bot.send_message(user_id, f"Group '{group_name}' does not exist.")
+
+def process_join_code_for_join(msg, user_id, username, group_name):
+    join_code = msg.text
+    if is_join_code_correct(group_name, join_code):
+        send_request_to_admin(group_name, user_id, username)
+    else:
+        bot.send_message(user_id, "Incorrect join code. Please try again.")
+
+def send_request_to_admin(group_name, user_id, username):
+    admin_id = get_admin_id(group_name)
+    if admin_id is not None:
+        notification_msg = f"User @{username} wants to join the group '{group_name}'. Do you approve?"  
+        bot.send_message(admin_id, notification_msg)
+
 
 #mapping
 mappings = {
